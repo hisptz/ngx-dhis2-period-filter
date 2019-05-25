860f793c2975e06f21955b9859123b5fc8f34727
Rajab Mkomwa <rajeyw@gmail.com>
2019-05-25 08:03:06 +0300

    fix(add-period-to-list): resolve issues on adding to empty period list

--- projects/ngx-dhis2-period-filter/src/lib/helpers /add-period-to-list.helper.ts
+++ projects/ngx-dhis2-period-filter/src/lib/helpers /add-period-to-list.helper.ts
@@ -3,6 +3,10 @@ export function addPeriodToList(periodList: any[], period: any) {
   if (!period) {
     return periodList;
   }
+
+  if (!periodList || periodList.length === 0) {
+    return [period];
+  }
   return (periodList || []).some(
     (periodItem: any) => periodItem.type === period.type
   )
