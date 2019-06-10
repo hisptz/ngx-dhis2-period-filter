f1a0ab42791edcb6fb422e6a952dee072fc48458
Rajab Mkomwa <rajeyw@gmail.com>
2019-06-10 13:55:32 +0300

    refactor: expose constants and helpers for public usage

--- projects/ngx-dhis2-period-filter/src/public-api.ts
+++ projects/ngx-dhis2-period-filter/src/public-api.ts
@@ -3,3 +3,31 @@
  */
 
 export * from './lib/ngx-dhis2-period-filter.module';
+
+// helpers
+export * from './lib/helpers/add-period-to-list.helper';
+export * from './lib/helpers/get-available-periods.helper';
+export * from './lib/helpers/get-bi-monthly-period.helper';
+export * from './lib/helpers/get-financial-april-periods.helper';
+export * from './lib/helpers/get-financial-july-periods.helper';
+export * from './lib/helpers/get-financial-october-periods.helper';
+export * from './lib/helpers/get-monthly-periods.helper';
+export * from './lib/helpers/get-quarterly-periods.helper';
+export * from './lib/helpers/get-six-monthly-april-periods.helper';
+export * from './lib/helpers/get-six-monthly-periods.helper';
+export * from './lib/helpers/get-yearly-periods.helper';
+export * from './lib/helpers/get-relative-bi-month-periods.helper';
+export * from './lib/helpers/get-relative-financial-year-periods.helper';
+export * from './lib/helpers/get-relative-month-periods.helper';
+export * from './lib/helpers/get-relative-quarter-periods.helper';
+export * from './lib/helpers/get-relative-six-month-periods.helper';
+export * from './lib/helpers/get-relative-week-periods.helper';
+export * from './lib/helpers/get-relative-year-periods.helper';
+export * from './lib/helpers/get-period-name.helper';
+export * from './lib/helpers/get-period-type.helper';
+export * from './lib/helpers/get-periods-based-on-type.helper';
+export * from './lib/helpers/get-sanitized-periods.helper';
+export * from './lib/helpers/remove-period-from-list.helper';
+
+// constants
+export * from './lib/constants/period-types.constant';
